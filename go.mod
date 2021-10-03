94c15a1f0ee39d937af2b38af6ad94c525b23085
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-10-03 10:46:16 +0800

    user with token and hass password

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/dinel13/wisata
 go 1.17
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 
-	github.com/lib/pq v1.10.3 
-)
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.3
+	github.com/pascaldekloe/jwt v1.10.0
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 
+	)
--- userHandler.go
+++ userHandler.go
@@ -3,12 +3,15 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/dinel13/wisata/models"
+	"github.com/pascaldekloe/jwt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type userPayload struct {
@@ -48,9 +51,18 @@ func (app *application) signupHandler(w http.ResponseWriter, r *http.Request) {
 	if i := strings.LastIndex(payload.Email, "@"); i > 0 {
 		name = payload.Email[:i]
 	}
+
+	// hash the password
+	passwrod, err := bcrypt.GenerateFromPassword([]byte(payload.Password), 14)
+	if err != nil {
+		log.Printf("Error hashing password: %v", err)
+		app.errorJSON(w, err, http.StatusInternalServerError)
+		return
+	}
+
 	user.Name = name
 	user.Email = payload.Email
-	user.Password = payload.Password
+	user.Password = string(passwrod)
 	user.CreatedAt = time.Now()
 	user.UpdatedAt = time.Now()
 
@@ -61,6 +73,38 @@ func (app *application) signupHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var claims jwt.Claims
+	claims.Subject = fmt.Sprint(newUser.ID)
+	claims.Issued = jwt.NewNumericTime(time.Now())
+	claims.NotBefore = jwt.NewNumericTime(time.Now())
+	claims.Expires = jwt.NewNumericTime(time.Now().Add(time.Hour * 24))
+	claims.Issuer = "test"
+	claims.Audiences = []string{"test"}
+
+	jwtBytes, err := claims.HMACSign(jwt.HS256, []byte(app.config.jwt.secret))
+	if err != nil {
+		app.errorJSON(w, errors.New("error signin"))
+		return
+	}
+
+	// return the user created with the token
+	type response struct {
+		Token string `json:"token"`
+		Name  string `json:"name"`
+		Email string `json:"email"`
+	}
+
+	resp := response{
+		Token: string(jwtBytes),
+		Name:  newUser.Name,
+		Email: newUser.Email,
+	}
+
 	// return the user
-	app.writeJSON(w, http.StatusOK, newUser, "user")
+	app.writeJSON(w, http.StatusOK, resp, "user")
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
